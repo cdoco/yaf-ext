09d30c02d22129fffbffacca99db8a67f636079b
GaoZiHang <gao_zihang@hotmail.com>
2015-02-06 10:11:12 +0800

    add ES

--- application/modules/Admin/views/templates_c/%%AA^AA1^AA1FC663%%Index.html.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2014-10-20 17:40:31
-         compiled from index%5CIndex.html */ ?>
-<?php echo $this->_tpl_vars['page']; ?>
--- application/modules/Admin/views/templates_c/%%E5^E54^E542C5B3%%index.html.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2014-10-20 09:46:28
-         compiled from index%5Cindex.html */ ?>
-<?php echo $this->_tpl_vars['page']; ?>
